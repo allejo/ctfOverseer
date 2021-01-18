07a24e0415f5e195ff8f0abbd89409ddc03fbc64
Vladimir Jimenez <allejo@me.com>
2021-01-17 21:55:09 -0800

    Assert that CTFOverseer is loaded in our extension plug-in first

--- ctfOverseerExtension.cpp
+++ ctfOverseerExtension.cpp
@@ -13,7 +13,11 @@ public:
 
 private:
     int onCaptureEventListenerID;
-    void OnCaptureEventCallbackV1(int playerID, bool isUnfair, bool wasDisallowed, bool isSelfCap);
+    void OnCaptureEventCallback(int playerID, bool isUnfair, bool wasDisallowed, bool isSelfCap);
+
+    const char* getCTFOverseerPlugin();
+    void registerCTFOverseerCallback();
+    void removeCTFOverseerCallback();
 };
 
 BZ_PLUGIN(SamplePlugin)
@@ -25,24 +29,62 @@ const char* SamplePlugin::Name()
 
 void SamplePlugin::Init(const char* config)
 {
-    using namespace std::placeholders;
-
-    std::string ctfOverseer = bz_getclipFieldString("allejo/ctfOverseer");
-    OnCaptureEventCallbackV1 callback = std::bind(&SamplePlugin::OnCaptureEventCallbackV1, this, _1, _2, _3, _4);
-    onCaptureEventListenerID = bz_callPluginGenericCallback(ctfOverseer.c_str(), "listenOnCaptureV1", static_cast<void*>(&callback));
-
-    bz_debugMessagef(0, "Event Listener registered: %d", onCaptureEventListenerID);
+    registerCTFOverseerCallback();
 }
 
 void SamplePlugin::Cleanup()
 {
     Flush();
 
-    std::string ctfOverseer = bz_getclipFieldString("allejo/ctfOverseer");
-    bz_callPluginGenericCallback(ctfOverseer.c_str(), "removeOnCapture", static_cast<int*>(&onCaptureEventListenerID));
+    removeCTFOverseerCallback();
 }
 
-void SamplePlugin::OnCaptureEventCallbackV1(int playerID, bool isUnfair, bool wasDisallowed, bool isSelfCap)
+const char* SamplePlugin::getCTFOverseerPlugin()
+{
+    const char* pluginName = bz_getclipFieldString("allejo/ctfOverseer");
+
+    if (pluginName == NULL)
+    {
+        bz_debugMessage(0, "ERROR! The plug-in for allejo/ctfOverseer could not be found.");
+    }
+    else if (!bz_pluginExists(pluginName))
+    {
+        bz_debugMessage(0, "ERROR! The required CTFOverseer plug-in is not loaded. Be sure it is loaded before this plug-in.");
+    }
+
+    return pluginName;
+}
+
+void SamplePlugin::registerCTFOverseerCallback()
+{
+    using namespace std::placeholders;
+
+    const char* ctfOverseer = getCTFOverseerPlugin();
+
+    if (ctfOverseer == NULL)
+    {
+        return;
+    }
+
+    OnCaptureEventCallbackV1 callback = std::bind(&SamplePlugin::OnCaptureEventCallback, this, _1, _2, _3, _4);
+    onCaptureEventListenerID = bz_callPluginGenericCallback(ctfOverseer, "listenOnCaptureV1", static_cast<void*>(&callback));
+
+    bz_debugMessagef(0, "Event Listener registered with ID: %d", onCaptureEventListenerID);
+}
+
+void SamplePlugin::removeCTFOverseerCallback()
+{
+    const char* ctfOverseer = getCTFOverseerPlugin();
+
+    if (ctfOverseer == NULL)
+    {
+        return;
+    }
+
+    bz_callPluginGenericCallback(ctfOverseer, "removeOnCapture", static_cast<int*>(&onCaptureEventListenerID));
+}
+
+void SamplePlugin::OnCaptureEventCallback(int playerID, bool isUnfair, bool wasDisallowed, bool isSelfCap)
 {
     bz_debugMessagef(0, "Player Capping: %d", playerID);
     bz_debugMessagef(0, "Was Unfair?: %s", isUnfair ? "true" : "false");
